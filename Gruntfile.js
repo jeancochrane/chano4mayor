a33475a37d194163178704c36bdc36a46eaf3912
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2017-03-09 16:59:51 -0500

    Set up grunt/sass etc.

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            self: {
+                files: 'Gruntfile.js'
+            },
+            
+            scss: {
+                files: 'src/scss/styles.scss',
+                tasks: ['sass', 'postcss']
+            },
+
+            css: {
+                files: 'styles.css',
+                options: {
+                    livereload: true
+                }
+            },
+
+            uglify: {
+                files: 'src/js/*.js',
+                tasks: ['uglify']
+            },
+
+            js: {
+                files: 'scripts.min.js',
+                options: {
+                    livereload: true
+                }
+            },
+
+            livereload: {
+                files: '**/*.{html,svg,png,jpg}',
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        sass: {
+            scss: {
+                options: {
+                    trace: true
+                },
+                src: 'src/scss/styles.scss',
+                dest: 'src/scss/styles-unprefixed.css'
+            }
+        },
+
+        postcss: {
+            css: {
+                options: {
+                    map: true,
+                    processors: [
+                        require('autoprefixer')({warnForDuplicates: false}),
+                        require('cssnano')()
+                    ]
+                },
+                src: 'src/scss/styles-unprefixed.css',
+                dest: 'styles.css'
+            }
+        },
+
+        uglify: {
+            options:{
+                compress: {
+                    drop_console: true
+                }
+            },
+            js: {
+                files: {
+                    'scripts.min.js': 'src/js/*.js'
+                }
+            }
+        },
+    });
+
+    grunt.registerTask('default', 'watch');
+};
