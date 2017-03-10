d19a0e423d2a650d83f63d3ce06cfa5ef35be028
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2017-03-10 12:45:32 -0500

    Run tasks at start

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,6 +78,6 @@ module.exports = function(grunt) {
             }
         },
     });
-
-    grunt.registerTask('default', 'watch');
+    grunt.registerTask('build', ['sass', 'postcss', 'uglify']);
+    grunt.registerTask('default', ['build', 'watch']);
 };
