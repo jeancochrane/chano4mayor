db50e31b29e119ddca6aec36c9f8a8a80f3f1dda
Jean Cochrane <jean@jeancochrane.com>
2018-10-29 19:14:36 -0400

    Try printing an env var in the lambda function

--- functions/tweetcount.js
+++ functions/tweetcount.js
@@ -1,6 +1,8 @@
 exports.handler = function(event, context, callback) {
+    var body = (process.env.TEST_VAR == null) ? 'Could not find variable TEST_VAR' : process.env.TEST_VAR;
+
     callback(null, {
         statusCode: 200,
-        body: 'Hello, world!'
+        body: body
     });
 }
