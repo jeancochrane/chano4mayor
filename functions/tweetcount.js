308ab8d4c056bcc6e375a3ac9701c7c7bac96ae6
Jean Cochrane <jean@jeancochrane.com>
2018-10-29 20:46:54 -0400

    Attempt a real Twitter API workflow in the lambda func

--- functions/tweetcount.js
+++ functions/tweetcount.js
@@ -1,8 +1,33 @@
-exports.handler = function(event, context, callback) {
-    var body = (process.env.TEST_VAR == null) ? 'Could not find variable TEST_VAR' : process.env.TEST_VAR;
+var https = require('https');
 
-    callback(null, {
-        statusCode: 200,
-        body: body
+exports.handler = function(event, context, callback) {
+
+    // This token should live as an environmental variable set in the
+    // build config for Netlify.
+    var bearerToken = process.env.TWITTER_BEARER_TOKEN;
+    if (bearerToken == null) {
+        callback('Could not find required TWITTER_BEARER_TOKEN environmental variable.')
+    }
+
+    var authToken = 'Bearer ' + bearerToken;
+    var queryString = '?q=chano4mayor%20OR%20chance4mayor%20OR%20chanoformayor%20OR%20chanceformayor';
+
+    var options = {
+        hostname: 'api.twitter.com',
+        path: '/1.1/search/tweets.json' + queryString,
+        headers: {
+            'Authorization': authToken;
+        }
+    };
+
+    https.get(options, function(res) {
+        res.on('data', function(data) {
+            callback(null, {
+                statusCode: 200,
+                body: data
+            })
+        }).on('error', function(err) {
+            callback(err);
+        });
     });
 }
